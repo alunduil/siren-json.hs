208089c77a65e5f55152007f81e000d78ea3a1a5
Alex Brandt <alunduil@alunduil.com>
2017-11-08 19:08:24 -0600

    drop use of isRight for older GHC versions

--- test/Internal/Network/HTTP/Types/Method/ArbitrarySpec.hs
+++ test/Internal/Network/HTTP/Types/Method/ArbitrarySpec.hs
@@ -8,10 +8,10 @@ Tests for "Internal.Network.HTTP.Types.Method.Arbitrary".
 -}
 module Internal.Network.HTTP.Types.Method.ArbitrarySpec (main, spec) where
 
-import Data.Either (isRight)
 import Network.HTTP.Types.Method (parseMethod, renderStdMethod)
-import Test.Hspec.QuickCheck (prop)
 import Test.Hspec (describe, hspec, Spec)
+import Test.Hspec.QuickCheck (prop)
+import Test.Invariant ((<=>))
 
 import Internal.Network.HTTP.Types.Method.Arbitrary ()
 
@@ -21,4 +21,4 @@ main = hspec spec
 spec :: Spec
 spec =
   describe "properties" $
-    prop "isRight . parseMethod . renderStdMethod" $ isRight . parseMethod . renderStdMethod
+    prop "parseMethod . renderStdMethod <=> Right" $ parseMethod . renderStdMethod <=> Right
