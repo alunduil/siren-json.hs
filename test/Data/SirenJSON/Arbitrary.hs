dd186b65ed7e01b41a520d2a8bb6f0ab2ad58b30
Alex Brandt <alunduil@alunduil.com>
2017-11-10 22:50:36 -0600

    add shrink methods to Arbitrary instances

--- test/Data/SirenJSON/Arbitrary.hs
+++ test/Data/SirenJSON/Arbitrary.hs
@@ -1,5 +1,7 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
+{-# LANGUAGE RecordWildCards #-}
+
 {-|
 Module      : Data.SirenJSON.Arbitrary
 Description : Arbitrary Instances for Data.SirenJSON
@@ -11,7 +13,8 @@ Arbitrary instances for "Data.SirenJSON".
 module Data.SirenJSON.Arbitrary where
 
 import Control.Applicative ((<$>), (<*>))
-import Test.QuickCheck (Arbitrary (arbitrary), elements, oneof, scale)
+import Data.Maybe (mapMaybe)
+import Test.QuickCheck (Arbitrary (arbitrary, shrink), elements, oneof, scale)
 import Test.QuickCheck.Instances ()
 
 import Data.SirenJSON
@@ -27,11 +30,23 @@ instance Arbitrary Entity where
                      <*> arbitrary
                      <*> arbitrary
 
+  shrink Entity{..} = mapMaybe e eEntities ++
+                      [ Entity eClass' eProperties' eEntities' eLinks' eActions' eTitle' | (eClass', eProperties', eEntities', eLinks', eActions', eTitle') <- shrink (eClass, eProperties, eEntities, eLinks, eActions, eTitle) ]
+    where e EmbeddedRepresentation{..} = Just sEntity
+          e _                          = Nothing
+
 instance Arbitrary SubEntity where
   arbitrary = oneof [ EmbeddedLink <$> arbitrary
                     , EmbeddedRepresentation <$> scale (`div` 2) arbitrary
                                              <*> arbitrary
                     ]
+
+  shrink (EmbeddedLink l)           = [ EmbeddedLink l' | l' <- shrink l ]
+  shrink EmbeddedRepresentation{..} = map EmbeddedLink (eLinks sEntity) ++
+                                      filter isEntity (eEntities sEntity) ++
+                                      [ EmbeddedRepresentation sEntity' sRel' | (sEntity', sRel') <- shrink (sEntity, sRel) ]
+    where isEntity (EmbeddedRepresentation _ _) = True
+          isEntity _                            = False
 
 instance Arbitrary Link where
   arbitrary = Link <$> arbitrary
@@ -39,6 +54,8 @@ instance Arbitrary Link where
                    <*> arbitrary
                    <*> arbitrary
                    <*> arbitrary
+
+  shrink Link{..} = [ Link lClass' lRel' lHref' lType' lTitle' | (lClass', lRel', lHref', lType', lTitle') <- shrink (lClass, lRel, lHref, lType, lTitle) ]
 
 instance Arbitrary Action where
   arbitrary = Action <$> arbitrary
@@ -49,12 +66,16 @@ instance Arbitrary Action where
                      <*> arbitrary
                      <*> arbitrary
 
+  shrink Action{..} = [ Action aName' aClass' aMethod' aHref' aTitle' aType' aFields' | (aName', aClass', aMethod', aHref', aTitle', aType', aFields') <- shrink (aName, aClass, aMethod, aHref, aTitle, aType, aFields) ]
+
 instance Arbitrary Field where
   arbitrary = Field <$> arbitrary
                     <*> arbitrary
                     <*> arbitrary
                     <*> arbitrary
                     <*> arbitrary
+
+  shrink Field{..} = [ Field fName' fClass' fType' fValue' fTitle' | (fName', fClass', fType', fValue', fTitle') <- shrink (fName, fClass, fType, fValue, fTitle) ]
 
 instance Arbitrary InputType where
   arbitrary = elements [ Hidden
