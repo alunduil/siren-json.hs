898e01becf5a0ab1d049ffbeddc5d56f885f7642
Alex Brandt <alunduil@alunduil.com>
2017-11-14 18:29:10 -0600

    add network-uri-json and refresh nix configuration

--- siren-json.nix
+++ siren-json.nix
@@ -1,22 +1,21 @@
-{ mkDerivation, aeson, base, bytestring, case-insensitive
-, containers, hspec, http-media, http-types, network-uri
-, QuickCheck, quickcheck-instances, stdenv, test-invariant, text
-, unordered-containers
-}:
-mkDerivation {
-  pname = "siren";
-  version = "0.1.0.0";
-  src = ./.;
-  libraryHaskellDepends = [
-    aeson base bytestring containers http-media http-types network-uri
-    text unordered-containers
-  ];
-  testHaskellDepends = [
-    aeson base bytestring case-insensitive containers hspec http-media
-    http-types network-uri QuickCheck quickcheck-instances
-    test-invariant text unordered-containers
-  ];
-  homepage = "https://github.com/alunduil/siren";
-  description = "Siren Tools for Haskell";
-  license = stdenv.lib.licenses.mit;
-}
+let
+  config = {
+    packageOverrides = pkgs: rec {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+
+          siren-json =
+            haskellPackagesNew.callPackage ./default.nix { };
+
+          network-uri-json =
+            haskellPackagesNew.callPackage ./network-uri-json.nix { };
+
+        };
+      };
+    };
+  };
+
+  pkgs = import <nixpkgs> { inherit config; };
+in
+  { siren-json = pkgs.haskellPackages.siren-json;
+  }
